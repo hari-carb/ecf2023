378eca8d91b693cbf620c248efc07669d0031605
Isabelle <isabelle.rol@yahoo.com>
2023-05-07 12:22:25 +0200

    Update menus to menus-carte - delete menus.php and nav-menu .php from templates

--- templates/nav-menu.php
+++
@@ -1,5 +0,0 @@
-<a href="menu.php?id=1">Menu Déjeuner</a>
-<a href="menu.php?id=2">Menu Duo</a>
-<a href="menu.php?id=3">Menu Végétarien</a>
-<a href="menu.php?id=4">Menu Dégustation</a>
-<a href="carte.php">La Carte</a>
